8bae1216469c9ec556504ec5d150a55693c0ca10
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-07-17 17:24:40 -0400

    created component folder & moved map into it

---
+++ src/components/Map.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+import './Map.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWpzaW0iLCJhIjoiY2thdmF0Ym9kMGNhbTJ6a3l5dzl3ZmdrcCJ9.YcSMrFRPTc7hCZgYkJeoSQ';
+
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -95,
+      lat: 54,
+      zoom: 3.8
+    };
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom
+    });
+    map.on('move', () => {
+      this.setState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <div ref={el => this.mapContainer = el} className="mapContainer" />
+      </div>
+    )
+  }
+}
+
+// ReactDOM.render(<Map />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import mapboxgl from 'mapbox-gl';
-import './App.css';
+import './components/Map.css';
+import Map from './components/Map.js'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWpzaW0iLCJhIjoiY2thdmF0Ym9kMGNhbTJ6a3l5dzl3ZmdrcCJ9.YcSMrFRPTc7hCZgYkJeoSQ';
 
 class Application extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      lng: 5,
-      lat: 34,
-      zoom: 2
-    };
-  }
-  componentDidMount() {
-    const map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
-    });
-  }
   render() {
     return (
       <div>
-        <div ref={el => this.mapContainer = el} className="mapContainer" />
+        <Map />
       </div>
     )
   }
