a1d3a3868c5ccdb2812b70f531147ddbef0fed1a
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-07-20 18:58:56 -0400

    started geojson creation service

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import './Map.css';
-import data from './data.json'
+// import data from './data.json'
 import real_data from './real_data.json'
+import createGeoJson from '../create-geojson.js'
 
 let addressArray = []
 real_data.forEach((item) => {
@@ -11,24 +12,93 @@ real_data.forEach((item) => {
 
 console.log(addressArray);
 
+var geojsonFeature = {
+  "type": "Feature",
+  "properties": {
+    "name": "Coors Field",
+    "amenity": "Baseball Stadium",
+    "popupContent": "This is where the Rockies play!"
+  },
+  "geometry": {
+    "type": "Point",
+    "coordinates": [-104.99404, 39.75621]
+  }
+};
+
 let coordinateArray = [];
 
-addressArray.forEach((address) => {
-  var request = new XMLHttpRequest();
-  request.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      var data = JSON.parse(request.responseText);
-      if (data[0] == undefined) {
-        console.error(`Geogratis failed to find coordinates for the following address: ${data[0]}`);
-      }
-      coordinateArray.push(data[0].geometry.coordinates);
+const testData = {
+  "type": "FeatureCollection",
+    "crs": {
+    "type": "name",
+      "properties": {
+      "name": "urn:ogc:def:crs:OGC:1.3:CRS84"
     }
-  };
-  request.open("GET", "https://www.geogratis.gc.ca/services/geolocation/en/locate?q=" + address, true);
-  request.send();
-});
+  },
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "ak16994521",
+        "mag": 2.3,
+        "time": 1507425650893,
+        "felt": null,
+        "tsunami": 0
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -151.5129,
+          63.1016,
+          0.0
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "id": "ak16994519",
+        "mag": 1.7,
+        "time": 1507425289659,
+        "felt": null,
+        "tsunami": 0
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -150.4048,
+          63.1224,
+          105.5
+        ]
+      }
+    }
+  ]
+}
 
-console.log(coordinateArray);
+
+// TODO: make this code asynchronous
+async function getCoordinates() {
+  try {
+    addressArray.forEach((address) => {
+      var request = new XMLHttpRequest();
+      request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          var data = JSON.parse(request.responseText);
+          if (data[0] == undefined) {
+            console.error(`Geogratis failed to find coordinates for the following address: ${data[0]}`);
+          }
+          coordinateArray.push(data[0].geometry.coordinates);
+        }
+      };
+      request.open("GET", "https://www.geogratis.gc.ca/services/geolocation/en/locate?q=" + address, true);
+      request.send();
+    });
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+getCoordinates().then(createGeoJson('abc'));
 
 // var request = new XMLHttpRequest();
 // request.onreadystatechange = function () {
@@ -51,7 +121,7 @@ export default class Map extends React.Component {
     this.state = {
       lng: -95,
       lat: 54,
-      zoom: 3.8
+      zoom: 3.8,
     };
   }
   componentDidMount() {
@@ -70,7 +140,7 @@ export default class Map extends React.Component {
         type: 'geojson',
         // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data,
+        data: testData,
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
---
+++ src/create-geojson.js
@@ -0,0 +1,4 @@
+export default function createGeoJson(data) {
+    console.log(`This is the data:`);
+    console.log(data)
+}
